d00c1a581e93f22944a9c9562d1ce8989a036606
Pavan Gowda T S <pavangowdats01@gmail.com>
2024-07-26 22:24:35 +0530

    last commit

--- script.js
+++ script.js
@@ -8,6 +8,63 @@ document.querySelector(".close-navbar-icon").addEventListener("click", () => {
   container.classList.remove("change");
 });
 
+document.querySelector(".list1").addEventListener("click", () => {
+  container.classList.remove("change");
+})
+
+document.querySelector(".list2").addEventListener("click", () => {
+  container.classList.remove("change");
+})
+
+document.querySelector(".list3").addEventListener("click", () => {
+  container.classList.remove("change");
+})
+
+document.querySelector(".list4").addEventListener("click", () => {
+  container.classList.remove("change");
+})
+
+document.querySelector(".list5").addEventListener("click", () => {
+  container.classList.remove("change");
+})
+
+
+document.querySelector(".reviews-button1").addEventListener("click", () => {
+  var reviewsSection = document.getElementById('reviews1');
+  if (reviewsSection) {  // Check if the element exists
+      if (reviewsSection.style.display === 'none' || reviewsSection.style.display === '') {
+          reviewsSection.style.display = 'block';
+      } else {
+          reviewsSection.style.display = 'none';
+      }
+  }
+});
+
+document.querySelector(".reviews-button2").addEventListener("click", () => {
+  var reviewsSection = document.getElementById('reviews2');
+  if (reviewsSection) {  // Check if the element exists
+      if (reviewsSection.style.display === 'none' || reviewsSection.style.display === '') {
+          reviewsSection.style.display = 'block';
+      } else {
+          reviewsSection.style.display = 'none';
+      }
+  }
+});
+
+document.querySelector(".reviews-button3").addEventListener("click", () => {
+  var reviewsSection = document.getElementById('reviews3');
+  if (reviewsSection) {  // Check if the element exists
+      if (reviewsSection.style.display === 'none' || reviewsSection.style.display === '') {
+          reviewsSection.style.display = 'block';
+      } else {
+          reviewsSection.style.display = 'none';
+      }
+  }
+});
+
+
+
+
 const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 
 let i = 0;
